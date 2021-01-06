4ba621f50f3e78906783aa358e6c10ca7abed44f
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-06 20:07:42 +0000

    Created a testing code using for loop and using range in loop.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-fruits=["Apple", "Peach", "Pear", "Peacan" ,"Chicken-Pot"]
-for fruit in fruits:
-  print(fruit)
-  print(fruit + " Pie") 
-  print(fruits)
+
+loop_test="testing testing ....."
+for i in range(10) :
+  print(loop_test)
+   
+  
